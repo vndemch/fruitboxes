9b01da7c5cc8639a8c1cff7e5f709588b44e8835
vndemch1 <vndemch@mail.ru>
2020-05-17 10:49:53 +0300

    Убрал num за ненадобностью

--- src/main/java/my/fruitbox/Box.java
+++ src/main/java/my/fruitbox/Box.java
@@ -4,35 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Box<T extends Fruit> {
-    private int num;
     private ArrayList<T> box;
 
     public Box(T... fruits) {
         box = new ArrayList<>(Arrays.asList(fruits));
-        this.num = box.size();
 
     }
 
     public void addItem (T... fruits) {
         System.out.println("Добавляем в коробку: "+fruits.length);
     box.addAll(Arrays.asList(fruits));
-    this.num = box.size();
     }
 
     public int getNum () {
-        return this.num;
+        return this.box.size();
     }
 
-    public void setEmpty () { box.clear(); this.num = 0; }
+    public void setEmpty () { box.clear(); }
 
     public void fromAnotherBox (Box<T> anotherBox) {
         if (box == anotherBox.box) {
             System.out.println("Это одна коробка");
             return;
         }
-        System.out.println("Перекладываем в коробку: " +anotherBox.box.size()+ " единиц");
+        System.out.println("Перекладываем в коробку: " +anotherBox.box.size()+ " ед.");
         box.addAll(anotherBox.box);
-        this.num += anotherBox.getNum();
         anotherBox.setEmpty();
     }
 }
